5599225b2e9c03061f0762e750317a37ea0f71e5
Arnab Banik <arnab.banik@optimizely.com>
2026-04-05 02:50:21 +0600

    (wezterm): add colorscheme

---
+++ .config/wezterm/colorscheme.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.apply(config)
+	config.color_scheme = "Flat (Gogh)"
+end
+
+return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 require("fonts").apply(config)
 require("window_appearance").apply(config)
 require("tab_bar").apply(config)
+require("colorscheme").apply(config)
 
 return config
