d1ae54bf65a28292486a5901203ded7922e1ddc6
Arnab Banik <arnab.banik@optimizely.com>
2026-04-05 02:52:15 +0600

    (wezterm)-fix: enable window resize
    RESIZE allows wezterm to fit the screen. It now uses the entire screen
    in both monitors. If 'None' was kept, then it would not fit the entire
    screen. (What a stupid option name from the developers)

--- .config/wezterm/window_appearance.lua
+++ .config/wezterm/window_appearance.lua
@@ -7,7 +7,7 @@ function M.apply(config)
 		top = 0,
 		bottom = 0,
 	}
-	config.window_decorations = "NONE"
+	config.window_decorations = "RESIZE"
 	config.window_frame = {
 		border_left_width = "0",
 		border_right_width = "0",
