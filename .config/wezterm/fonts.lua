9ec09006dd0dc5f7237920e839682f756ee9928a
Arnab Banik <arnab.banik@optimizely.com>
2026-04-04 18:14:18 +0600

    (wezterm): set up fonts for wezterm

---
+++ .config/wezterm/fonts.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+local M = {}
+
+function M.apply(config)
+	config.font_size = 14
+	config.line_height = 1.1
+	config.font = wezterm.font("JetBrainsMono Nerd Font")
+end
+
+return M
