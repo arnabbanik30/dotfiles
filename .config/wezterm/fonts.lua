820ecfa7e320626ed74843867435ad0ba19e7513
Arnab Banik <arnab.banik@optimizely.com>
2026-04-05 16:14:33 +0600

    (wezterm): use thicker font

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.apply(config)
 	config.font_size = 14
 	config.line_height = 1.1
-	config.font = wezterm.font("JetBrainsMono Nerd Font")
+	config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "DemiBold" })
 end
 
 return M
